4bfeffe2cc65f4036c057e2100b5a0eaea5c41c3
Daniel Wertheim <daniel@wertheim.se>
2013-04-20 23:00:24 +0200

    Added UnitTests project.

--- src/Tests/MyCouch.Testing/GlobalTestConstants.cs
+++ src/Tests/MyCouch.Testing/GlobalTestConstants.cs
@@ -1,6 +1,6 @@
 ﻿namespace MyCouch.Testing
 {
-    public static class TestConstants
+    public static class GlobalTestConstants
     {
         //public static readonly DateTime FixedDateTime = new DateTime(2013, 01, 01, 13,33,40);
     }
---
+++ src/Tests/MyCouch.Testing/TestDataFactory.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MyCouch.Testing.Model;
+
+namespace MyCouch.Testing
+{
+    public static class TestDataFactory
+    {
+        public static Artist CreateArtist()
+        {
+            return CreateArtists(1).Single();
+        }
+
+        public static Artist[] CreateArtists(int numOf)
+        {
+            var artists = new List<Artist>();
+            var numOfAlbums = new[] { 1, 2, 3 };
+
+            for (var c = 0; c < numOf; c++)
+            {
+                var artist = new Artist
+                {
+                    Name = "Artist #" + numOf
+                };
+
+                artist.Albums = CreateAlbums(numOfAlbums[c % numOfAlbums.Length], artist.Name);
+
+                artists.Add(artist);
+            }
+
+            return artists.ToArray();
+        }
+
+        private static Album[] CreateAlbums(int numOf, string artistName)
+        {
+            var albums = new List<Album>();
+
+            for (var c = 0; c < numOf; c++)
+            {
+                albums.Add(new Album
+                {
+                    Name = string.Format("{0} - Greatest fakes #{1}", artistName, c + 1)
+                });
+            }
+
+            return albums.ToArray();
+        }
+    }
+}
