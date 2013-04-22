1434e4ef473f5dac5b690b5f6f1fdae7fc5511ed
Daniel Wertheim <daniel@wertheim.se>
2013-04-22 23:53:31 +0200

    Fixed deserialization. Still need support for ViewResponse of strings.

--- src/Projects/MyCouch/Serialization/MyCouchSerializer.cs
+++ src/Projects/MyCouch/Serialization/MyCouchSerializer.cs
@@ -1,7 +1,11 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 using MyCouch.Schemes;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 
 namespace MyCouch.Serialization
 {
@@ -9,25 +13,22 @@ namespace MyCouch.Serialization
     {
         protected readonly IEntityAccessor EntityAccessor;
         protected readonly JsonSerializer Serializer;
+        protected readonly JsonSerializer Deserializer;
 
         public MyCouchSerializer(IEntityAccessor entityAccessor)
         {
             EntityAccessor = entityAccessor;
-            Serializer = CreateSerializer();
+            Serializer = JsonSerializer.Create(CreateSettings(new SerializationContractResolver(EntityAccessor)));
+            Deserializer = JsonSerializer.Create(CreateSettings(new DefaultContractResolver(true)));
         }
 
-        protected virtual JsonSerializer CreateSerializer()
-        {
-            return JsonSerializer.Create(CreateSettings());
-        }
-
-        protected virtual JsonSerializerSettings CreateSettings()
+        protected virtual JsonSerializerSettings CreateSettings(IContractResolver contractResolver)
         {
             return new JsonSerializerSettings
             {
                 TypeNameHandling = TypeNameHandling.None,
                 ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor,
-                ContractResolver = new SerializationContractResolver(EntityAccessor),
+                ContractResolver = contractResolver,
                 DateFormatHandling = DateFormatHandling.IsoDateFormat,
                 DateTimeZoneHandling = DateTimeZoneHandling.RoundtripKind,
                 Formatting = Formatting.None,
@@ -67,7 +68,7 @@ namespace MyCouch.Serialization
             {
                 using (var jsonReader = new JsonTextReader(reader))
                 {
-                    return Serializer.Deserialize<T>(jsonReader);
+                    return Deserializer.Deserialize<T>(jsonReader);
                 }
             }
         }
@@ -81,7 +82,7 @@ namespace MyCouch.Serialization
             {
                 using (var jsonReader = new JsonTextReader(reader))
                 {
-                    return Serializer.Deserialize<T>(jsonReader);
+                    return Deserializer.Deserialize<T>(jsonReader);
                 }
             }
         }
@@ -126,37 +127,123 @@ namespace MyCouch.Serialization
                 {
                     while (jr.Read())
                     {
-                        if (jr.TokenType == JsonToken.PropertyName)
+                        if (jr.TokenType != JsonToken.PropertyName) 
+                            continue;
+                        
+                        var propName = jr.Value.ToString();
+                        if (propName == "total_rows")
                         {
-                            var propName = jr.Path;
-                            if (propName == "total_rows")
-                            {
-                                if (!jr.Read())
-                                    break;
-                                response.TotalRows = (long)jr.Value;
-                                continue;
-                            }
+                            if (!jr.Read())
+                                break;
+                            response.TotalRows = (long)jr.Value;
+                            continue;
+                        }
 
-                            if (propName == "offset")
-                            {
-                                if (!jr.Read())
-                                    break;
-                                response.OffSet = (long)jr.Value;
-                                continue;
-                            }
+                        if (propName == "offset")
+                        {
+                            if (!jr.Read())
+                                break;
+                            response.OffSet = (long)jr.Value;
+                            continue;
+                        }
 
-                            if (propName == "rows")
-                            {
-                                if (!jr.Read())
-                                    break;
+                        if (propName == "rows")
+                        {
+                            if (!jr.Read())
+                                break;
 
-                                response.Rows = Serializer.Deserialize<ViewQueryResponse<T>.Row[]>(jr);
+                            if (response is ViewQueryResponse<string>)
+                                response.Rows = YieldViewQueryStringResponseRows<T>(jr).ToArray();
+                            else if (response is ViewQueryResponse<string[]>)
+                            {
+                                //TODO: Fix
                             }
+                            else
+                                response.Rows = Deserializer.Deserialize<ViewQueryResponse<T>.Row[]>(jr);
                         }
                     }
                 }
             }
         }
+
+        protected IEnumerable<ViewQueryResponse<T>.Row> YieldViewQueryStringResponseRows<T>(JsonReader jr) where T : class
+        {
+            if (jr.TokenType != JsonToken.StartArray)
+                yield break;
+
+            var row = new ViewQueryResponse<T>.Row();
+            var startDepth = jr.Depth;
+            var sb = new StringBuilder();
+            var hasMappedId = false;
+            var hasMappedKey = false;
+            var hasMappedValue = false;
+            using (var sw = new StringWriter(sb))
+            {
+                using (var jw = new JsonTextWriter(sw))
+                {
+                    while (jr.Read() && !(jr.TokenType == JsonToken.EndArray && jr.Depth == startDepth))
+                    {
+                        if (jr.TokenType != JsonToken.PropertyName) 
+                            continue;
+
+                        var propName = jr.Value.ToString().ToLower();
+                        if (propName == "id")
+                        {
+                            if (!jr.Read())
+                                break;
+                            row.Id = jr.Value.ToString();
+                            hasMappedId = true;
+                        }
+                        else if (propName == "key")
+                        {
+                            if (!jr.Read())
+                                break;
+                            row.Key = jr.Value.ToString();
+                            hasMappedKey = true;
+                        }
+                        else if (propName == "value")
+                        {
+                            if (!jr.Read())
+                                break;
+                            jw.WriteToken(jr, true);
+                            row.Value = sb.ToString() as T;
+                            sb.Clear();
+                            hasMappedValue = true;
+                        }
+                        else
+                            continue;
+
+                        if (hasMappedId && hasMappedKey && hasMappedValue)
+                        {
+                            hasMappedId = hasMappedKey = hasMappedValue = false;
+                            yield return row;
+                            row = new ViewQueryResponse<T>.Row();
+                        }
+                    }
+                }
+            }
+        }
+
+        //protected IEnumerable<string> YieldViewQueryResponseRows(JsonReader jr)
+        //{
+        //    if(jr.TokenType != JsonToken.StartArray)
+        //        yield break;
+
+        //    var sb = new StringBuilder();
+        //    using (var sw = new StringWriter(sb))
+        //    {
+        //        using (var jw = new JsonTextWriter(sw))
+        //        {
+        //            var startDepth = jr.Depth;
+        //            while (jr.Read() && !(jr.TokenType == JsonToken.EndArray && jr.Depth == startDepth))
+        //            {
+        //                jw.WriteToken(jr, true);
+        //                yield return sb.ToString();
+        //                sb.Clear();
+        //            }
+        //        }
+        //    }
+        //}
 
         public virtual void PopulateFailedResponse<T>(T response, Stream data) where T : Response
         {
