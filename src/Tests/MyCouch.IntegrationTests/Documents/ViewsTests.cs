ead7b5eaddf54316509b058b420770901ae5b37f
Daniel Wertheim <daniel@wertheim.se>
2013-04-24 21:27:11 +0200

    ViewsTests skeleton

---
+++ src/Tests/MyCouch.IntegrationTests/Documents/ViewsTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace MyCouch.IntegrationTests.Documents
+{
+    [TestFixture]
+    public class ViewsTests : IntegrationTestsOf<IViews>
+    {
+        protected override void OnTestInitialize()
+        {
+            base.OnTestInitialize();
+
+            SUT = Client.Views;
+        }
+
+        protected override void OnTestFinalize()
+        {
+            base.OnTestFinalize();
+
+            IntegrationTestsRuntime.ClearAllDocuments();
+        }
+    }
+}
