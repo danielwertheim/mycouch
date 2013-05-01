1e263fb5524144458bc65a19f3097e2713b14aba
Daniel Wertheim <daniel@wertheim.se>
2013-05-01 17:38:46 +0200

    Support for basic authentication via url.

--- src/Projects/MyCouch/Net/BasicHttpClientConnection.cs
+++ src/Projects/MyCouch/Net/BasicHttpClientConnection.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using EnsureThat;
+using MyCouch.Extensions;
+using MyCouch.Resources;
 
 namespace MyCouch.Net
 {
@@ -17,8 +21,6 @@ namespace MyCouch.Net
 
         public BasicHttpClientConnection(Uri uri)
         {
-            Ensure.That(uri, "uri").IsNotNull();
-
             HttpClient = CreateHttpClient(uri);
         }
 
@@ -32,10 +34,32 @@ namespace MyCouch.Net
             HttpClient = null;
         }
 
+        protected virtual void EnsureValidUri(Uri uri)
+        {
+            Ensure.That(uri, "uri").IsNotNull();
+            Ensure.That(uri.LocalPath, "uri.LocalPath")
+                .IsNotNullOrEmpty()
+                .WithExtraMessageOf(() => ExceptionStrings.BasicHttpClientConnection_UriIsMissingDb);
+        }
+
         protected virtual HttpClient CreateHttpClient(Uri uri)
         {
-            var client = new HttpClient { BaseAddress = uri };
+            EnsureValidUri(uri);
+
+            var url = string.Format("{0}://{1}{2}", uri.Scheme, uri.Authority, uri.LocalPath);
+
+            var client = new HttpClient { BaseAddress = new Uri(url) };
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(HttpContentTypes.Json));
+
+            if (!string.IsNullOrWhiteSpace(uri.UserInfo))
+            {
+                var parts = uri.UserInfo
+                    .Split(new[] { ":" }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(p => Uri.UnescapeDataString(p))
+                    .ToArray();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", string.Join(":", parts).AsBase64Encoded());
+            }
 
             return client;
         }
