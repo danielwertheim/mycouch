fc259b0e241810aeadf435257b282d209be90284
Daniel Wertheim <daniel@wertheim.se>
2013-05-02 21:18:16 +0200

    Rearranged

--- src/Tests/MyCouch.IntegrationTests/Documents/DocumentsTests.cs
+++ src/Tests/MyCouch.IntegrationTests/Documents/DocumentsTests.cs
@@ -119,34 +119,36 @@ namespace MyCouch.IntegrationTests.Documents
         [Test]
         public void CRUD_using_non_typed_API()
         {
-            var post1 = SUT.PostAsync(TestData.Artists.Artist1Json).Result;
-            post1.Should().BeSuccessfulPost(TestData.Artists.Artist1Id);
-
+            var post1 = SUT.PostAsync(TestData.Artists.Artist1Json);
             var post2 = SUT.Post(TestData.Artists.Artist2Json);
+
+            post1.Result.Should().BeSuccessfulPost(TestData.Artists.Artist1Id);
             post2.Should().BeSuccessfulPost(TestData.Artists.Artist2Id);
 
-            var get1 = SUT.GetAsync(post1.Id).Result;
-            get1.Should().BeSuccessfulGet(post1.Id);
-
+            var get1 = SUT.GetAsync(post1.Result.Id);
             var get2 = SUT.Get(post2.Id);
+
+            get1.Result.Should().BeSuccessfulGet(post1.Result.Id);
             get2.Should().BeSuccessfulGet(post2.Id);
 
-            var kv1 = Client.Serializer.Deserialize<IDictionary<string, dynamic>>(get1.Content);
+            var kv1 = Client.Serializer.Deserialize<IDictionary<string, dynamic>>(get1.Result.Content);
             kv1["year"] = 2000;
             var docUpd1 = Client.Serializer.Serialize(kv1);
-            var put1 = SUT.PutAsync(get1.Id, docUpd1).Result;
-            put1.Should().BeSuccessfulPut(get1.Id);
+
+            var put1 = SUT.PutAsync(get1.Result.Id, docUpd1);
 
             var kv2 = Client.Serializer.Deserialize<IDictionary<string, dynamic>>(get2.Content);
             kv2["year"] = 2001;
             var docUpd2 = Client.Serializer.Serialize(kv2);
             var put2 = SUT.Put(get2.Id, docUpd2);
+
+            put1.Result.Should().BeSuccessfulPut(get1.Result.Id);
             put2.Should().BeSuccessfulPut(get2.Id);
 
-            var delete1 = SUT.DeleteAsync(put1.Id, put1.Rev).Result;
-            delete1.Should().BeSuccessfulDelete(put1.Id);
-
+            var delete1 = SUT.DeleteAsync(put1.Result.Id, put1.Result.Rev);
             var delete2 = SUT.Delete(put2.Id, put2.Rev);
+
+            delete1.Result.Should().BeSuccessfulDelete(put1.Result.Id);
             delete2.Should().BeSuccessfulDelete(put2.Id);
         }
 
@@ -157,28 +159,28 @@ namespace MyCouch.IntegrationTests.Documents
             var artist1 = artists[0];
             var artist2 = artists[1];
 
-            var post1 = SUT.PostAsync(artist1).Result;
-            post1.Should().BeSuccessfulPost(artist1.ArtistId, e => e.ArtistId, e => e.ArtistRev);
-
+            var post1 = SUT.PostAsync(artist1);
             var post2 = SUT.Post(artist2);
+
+            post1.Result.Should().BeSuccessfulPost(artist1.ArtistId, e => e.ArtistId, e => e.ArtistRev);
             post2.Should().BeSuccessfulPost(artist2.ArtistId, e => e.ArtistId, e => e.ArtistRev);
 
-            var get1 = SUT.GetAsync<Artist>(post1.Id).Result;
-            get1.Should().BeSuccessfulGet(post1.Id);
-
+            var get1 = SUT.GetAsync<Artist>(post1.Result.Id);
             var get2 = SUT.Get<Artist>(post2.Id);
+
+            get1.Result.Should().BeSuccessfulGet(post1.Result.Id);
             get2.Should().BeSuccessfulGet(post2.Id);
 
-            get1.Entity.Albums = new List<Album>(get1.Entity.Albums) { new Album { Name = "Test" } }.ToArray();
-            var put1 = SUT.PutAsync(get1.Entity).Result;
-
+            get1.Result.Entity.Albums = new List<Album>(get1.Result.Entity.Albums) { new Album { Name = "Test" } }.ToArray();
             get2.Entity.Albums = new List<Album>(get2.Entity.Albums) { new Album { Name = "Test" } }.ToArray();
+
+            var put1 = SUT.PutAsync(get1.Result.Entity);
             var put2 = SUT.Put(get2.Entity);
 
-            var delete1 = SUT.DeleteAsync(put1.Entity).Result;
-            delete1.Should().BeSuccessfulDelete(put1.Id, e => e.ArtistId, e => e.ArtistRev);
-
+            var delete1 = SUT.DeleteAsync(put1.Result.Entity);
             var delete2 = SUT.Delete(put2.Entity);
+
+            delete1.Result.Should().BeSuccessfulDelete(put1.Result.Id, e => e.ArtistId, e => e.ArtistRev);
             delete2.Should().BeSuccessfulDelete(put2.Id, e => e.ArtistId, e => e.ArtistRev);
         }
     }
