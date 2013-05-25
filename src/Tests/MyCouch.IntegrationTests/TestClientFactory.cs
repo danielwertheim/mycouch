aab810f233c6211b4106f8bc06c018a31827cff8
Daniel Wertheim <daniel@wertheim.se>
2013-05-25 22:20:43 +0200

    Use at least a password that has char that needs to be encoded so that that is tested.

--- src/Tests/MyCouch.IntegrationTests/TestClientFactory.cs
+++ src/Tests/MyCouch.IntegrationTests/TestClientFactory.cs
@@ -1,10 +1,12 @@
-﻿namespace MyCouch.IntegrationTests
+﻿using System;
+
+namespace MyCouch.IntegrationTests
 {
     internal static class TestClientFactory
     {
         internal static IClient CreateDefault()
         {
-            return new Client("http://mycouchtester:1q2w3e4r@localhost:5984/" + TestConstants.TestDbName);
+            return new Client("http://mycouchtester:" + Uri.EscapeDataString("p@ssword") + "@localhost:5984/" + TestConstants.TestDbName);
         }
     }
 }
