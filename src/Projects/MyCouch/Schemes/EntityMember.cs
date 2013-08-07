0e225e04d0b4b9accf1c824e0ee05669e23c5ba0
Daniel Wertheim <daniel@wertheim.se>
2013-08-07 13:36:43 +0200

    The reflection stuff for accessing values is only used for extracting document id and revs for entities, hence it could be in Schemes.

--- src/Projects/MyCouch/Schemes/EntityMember.cs
+++ src/Projects/MyCouch/Schemes/EntityMember.cs
@@ -3,7 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using MyCouch.Reflections;
+using MyCouch.Schemes.Reflections;
 
 namespace MyCouch.Schemes
 {
---
+++ src/Projects/MyCouch/Schemes/Reflections/DynamicProperty.cs
@@ -0,0 +1,14 @@
+﻿namespace MyCouch.Schemes.Reflections
+{
+    public class DynamicProperty
+    {
+        public DynamicGetter Getter { get; private set; }
+        public DynamicSetter Setter { get; private set; }
+
+        public DynamicProperty(DynamicGetter getter, DynamicSetter setter)
+        {
+            Getter = getter;
+            Setter = setter;
+        }
+    }
+}
