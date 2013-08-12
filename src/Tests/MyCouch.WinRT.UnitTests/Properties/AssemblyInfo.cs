f2109e286ff638d862158c7ab6ae006b240c8670
Daniel Wertheim <daniel@wertheim.se>
2013-08-12 23:46:43 +0200

    Started on replacing NUnit with MSTest

--- src/Tests/MyCouch.WinRT.UnitTests/Properties/AssemblyInfo.cs
+++ src/Tests/MyCouch.WinRT.UnitTests/Properties/AssemblyInfo.cs
@@ -1,10 +1,3 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("MyCouch.WinRT.UnitTests")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
