6c27590621f0318ae3012cf5a745da1b6196b921
Daniel Wertheim <daniel@wertheim.se>
2013-08-15 20:06:20 +0200

    Testing lib for WinRT should not be test project, just a plain lib.

--- src/Tests/MyCouch.Testing/JsonTestData.cs
+++ src/Tests/MyCouch.Testing/JsonTestData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 #if WinRT
 using Windows.ApplicationModel;
@@ -38,7 +39,7 @@ namespace MyCouch.Testing
         private static string ReadFile(string name)
         {
 #if !WinRT
-            var filePath = Path.Combine(GenerateFilesFolderName(), name);
+            var filePath = Path.Combine(string.Concat(typeof(JsonTestData).Name, "Files"), name);
 
             return Cache.GetOrAdd(name, File.ReadAllText(filePath));
 #else
@@ -48,15 +49,11 @@ namespace MyCouch.Testing
 #if WinRT
         private static async Task<string> WinRtStyleOfDoingASimpleRead(string name)
         {
-            var folder = await Package.Current.InstalledLocation.GetFolderAsync(GenerateFilesFolderName());
+            var folder = await Package.Current.InstalledLocation.GetFolderAsync(Path.Combine(typeof(JsonTestData).GetTypeInfo().Assembly.GetName().Name, string.Concat(typeof(JsonTestData).Name, "Files")));
             var file = await folder.GetFileAsync(name);
             
             return await FileIO.ReadTextAsync(file);
         }
 #endif
-        private static string GenerateFilesFolderName()
-        {
-            return string.Concat(typeof(JsonTestData).Name, "Files");
-        }
     }
 }
--- src/Tests/MyCouch.WinRT.Testing/Properties/AssemblyInfo.cs
+++ src/Tests/MyCouch.WinRT.Testing/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MyCouch.WinRT.Testing")]
+[assembly: ComVisible(false)]
