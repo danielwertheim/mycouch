0de0095fb6ee69641081adc6b4b86f5922445c1b
Daniel Wertheim <daniel@wertheim.se>
2013-08-20 13:33:42 +0200

    MyCouch.Rich which will hold extended stuff.

---
+++ src/Projects/MyCouch.Rich/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MyCouch.Rich")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0d81bdd1-bb48-4987-8d93-969b496fbecc")]
