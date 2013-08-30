c021680553ffa29c623418d1cfa246189f350c62
Daniel Wertheim <daniel@wertheim.se>
2013-08-30 16:23:17 +0200

    Search
    All set. Ready to start.

--- src/Tests/MyCouch.IntegrationTests/CloudantTests/SearchTests.cs
+++ src/Tests/MyCouch.IntegrationTests/CloudantTests/SearchTests.cs
@@ -1,12 +1,22 @@
 ﻿using MyCouch.Cloudant;
+using MyCouch.IntegrationTests.TestFixtures;
+using MyCouch.Testing.Model;
+using Xunit;
 
 namespace MyCouch.IntegrationTests.CloudantTests
 {
-    public class SearchTests : CloudantTestsOf<ISearch>
+    public class SearchTests : CloudantTestsOf<ISearch>, IPreserveStatePerFixture, IUseFixture<SearchFixture>
     {
+        protected Animal[] Animals { get; set; }
+
         protected override void OnTestInit()
         {
             SUT = Client.Search;
+        }
+
+        public void SetFixture(SearchFixture data)
+        {
+            Animals = data.Animals;
         }
     }
 }
