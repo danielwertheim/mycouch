91ce4cd93e527b3a91075a8f9ccfac9f23aeec8d
Daniel Wertheim <daniel@wertheim.se>
2013-09-30 19:48:01 +0200

    Changes, not started yet.

--- src/Projects/MyCouch/Contexts/Changes.cs
+++ src/Projects/MyCouch/Contexts/Changes.cs
@@ -1,16 +1,16 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using EnsureThat;
 using MyCouch.Commands;
 using MyCouch.Extensions;
-using MyCouch.Net;
 using MyCouch.Responses;
 
 namespace MyCouch.Contexts
 {
     public class Changes : ApiContextBase, IChanges
     {
-        public Changes(IConnection connection) : base(connection) { }
+        public Changes(IConnection connection) : base(connection) {}
 
         public virtual Task<ChangesResponse> GetAsync(ChangesFeed feed)
         {
@@ -32,21 +32,12 @@ namespace MyCouch.Contexts
 
         protected virtual HttpRequestMessage CreateRequest(GetChangesCommand cmd)
         {
-            return new HttpRequest(HttpMethod.Get, GenerateRequestUrl(cmd));
-        }
-
-        protected virtual string GenerateRequestUrl(GetChangesCommand cmd)
-        {
-            var querystring = "";
-
-            return string.Format("{0}/_changes?{1}",
-                Connection.Address,
-                querystring);
+            throw new NotImplementedException();
         }
 
         protected virtual ChangesResponse ProcessHttpResponse(HttpResponseMessage response)
         {
-            return null;
+            throw new NotImplementedException();
         }
     }
 }
