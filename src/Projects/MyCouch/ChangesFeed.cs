b3383e832654fe3f3b120011a4ebcc7c8e832c7b
Daniel Wertheim <daniel@wertheim.se>
2013-10-03 00:32:40 +0200

    ChangesFeed is now a key-master class.

--- src/Projects/MyCouch/ChangesFeed.cs
+++ src/Projects/MyCouch/ChangesFeed.cs
@@ -1,14 +1,56 @@
 ﻿using System;
+using EnsureThat;
 
 namespace MyCouch
 {
-#if !NETFX_CORE
-    [Serializable]
-#endif
-    public enum ChangesFeed
+    public class ChangesFeed : IEquatable<ChangesFeed>, IEquatable<string>
     {
-        Normal = 0,
-        Longpoll = 1,
-        Continuous = 2
+        protected readonly string Value;
+
+        private static readonly ChangesFeed NormalValue = new ChangesFeed("normal");
+        private static readonly ChangesFeed LongpollValue = new ChangesFeed("longpoll");
+        private static readonly ChangesFeed ContinuousValue = new ChangesFeed("continuous");
+
+        public static ChangesFeed Normal { get { return NormalValue; } }
+        public static ChangesFeed Longpoll { get { return LongpollValue; } }
+        public static ChangesFeed Continuous { get { return ContinuousValue; } }
+
+        private ChangesFeed(string value)
+        {
+            Ensure.That(value, "value").IsNotNull();
+            Value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as ChangesFeed);
+        }
+
+        public bool Equals(ChangesFeed other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return string.Equals(Value, other.Value);
+        }
+
+        public bool Equals(string other)
+        {
+            return string.Equals(Value, other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public static implicit operator string(ChangesFeed item)
+        {
+            return item.Value;
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
     }
 }
