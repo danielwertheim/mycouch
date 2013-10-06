4f89de81a4cadc79b9d070d673b418ada2af9461
Will Holley <willholley@gmail.com>
2013-10-06 22:00:07 +0100

    Add http request factory for attachments.
    Add a HttpRequestFactory for GET, PUT and DELETE attachment requests.

---
+++ src/Projects/MyCouch/Requests/Factories/AttachmentHttpRequestFactory.cs
@@ -0,0 +1,59 @@
+﻿using MyCouch.Net;
+using MyCouch.Requests;
+using MyCouch.Requests.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace MyCouch.Contexts
+{
+    public class AttachmentHttpRequestFactory : 
+        HttpRequestFactoryBase,
+        IHttpRequestFactory<GetAttachmentRequest>,
+        IHttpRequestFactory<PutAttachmentRequest>,
+        IHttpRequestFactory<DeleteAttachmentRequest>
+    {
+        public AttachmentHttpRequestFactory(IConnection connection) : base(connection)
+        {
+        }
+
+        protected virtual string GenerateRequestUrl(string docId, string docRev, string attachmentName)
+        {
+            return string.Format("{0}/{1}/{2}{3}",
+                Connection.Address,
+                docId,
+                attachmentName,
+                docRev == null ? string.Empty : string.Concat("?rev=", docRev));
+        }
+
+        public HttpRequest Create(GetAttachmentRequest request)
+        {
+            var httpRequest = new HttpRequest(HttpMethod.Get, GenerateRequestUrl(request.DocId, request.DocRev, request.Name));
+
+            httpRequest.SetIfMatch(request.DocRev);
+
+            return httpRequest;
+        }
+
+        public HttpRequest Create(PutAttachmentRequest request)
+        {
+            var httpRequest = new HttpRequest(HttpMethod.Put, GenerateRequestUrl(request.DocId, request.DocRev, request.Name));
+
+            httpRequest.SetIfMatch(request.DocRev);
+            httpRequest.SetContent(request.ContentType, request.Content);
+
+            return httpRequest;
+        }
+
+        public HttpRequest Create(DeleteAttachmentRequest request)
+        {
+            var httpRequest = new HttpRequest(HttpMethod.Delete, GenerateRequestUrl(request.DocId, request.DocRev, request.Name));
+
+            httpRequest.SetIfMatch(request.DocRev);
+
+            return httpRequest;
+        }
+    }
+}
