bdafceb3418ca03fc5c6f332f198a6d8e044bcb8
Daniel Wertheim <daniel@wertheim.se>
2013-10-15 20:11:06 +0200

    ExecuteAsync is now PerformAsync.

--- src/Projects/MyCouch/ClientExecuteExtensions.cs
+++ src/Projects/MyCouch/ClientExecuteExtensions.cs
@@ -6,67 +6,67 @@ namespace MyCouch
 {
     public static class ClientExecuteExtensions
     {
-        public static Task<ChangesResponse> ExecuteAsync(this IClient client, GetChangesRequest request)
+        public static Task<ChangesResponse> PerformAsync(this IClient client, GetChangesRequest request)
         {
             return client.Changes.GetAsync(request);
         }
 
-        public static Task<BulkResponse> ExecuteAsync(this IClient client, BulkRequest request)
+        public static Task<BulkResponse> PerformAsync(this IClient client, BulkRequest request)
         {
             return client.Documents.BulkAsync(request);
         }
 
-        public static Task<DocumentHeaderResponse> ExecuteAsync(this IClient client, CopyDocumentRequest request)
+        public static Task<DocumentHeaderResponse> PerformAsync(this IClient client, CopyDocumentRequest request)
         {
             return client.Documents.CopyAsync(request);
         }
 
-        public static Task<DocumentHeaderResponse> ExecuteAsync(this IClient client, ReplaceDocumentRequest request)
+        public static Task<DocumentHeaderResponse> PerformAsync(this IClient client, ReplaceDocumentRequest request)
         {
             return client.Documents.ReplaceAsync(request);
         }
 
-        public static Task<DocumentHeaderResponse> ExecuteAsync(this IClient client, DocumentExistsRequest request)
+        public static Task<DocumentHeaderResponse> PerformAsync(this IClient client, DocumentExistsRequest request)
         {
             return client.Documents.ExistsAsync(request);
         }
 
-        public static Task<DocumentResponse> ExecuteAsync(this IClient client, GetDocumentRequest request)
+        public static Task<DocumentResponse> PerformAsync(this IClient client, GetDocumentRequest request)
         {
             return client.Documents.GetAsync(request);
         }
 
-        public static Task<DocumentHeaderResponse> ExecuteAsync(this IClient client, PostDocumentRequest request)
+        public static Task<DocumentHeaderResponse> PerformAsync(this IClient client, PostDocumentRequest request)
         {
             return client.Documents.PostAsync(request);
         }
 
-        public static Task<DocumentHeaderResponse> ExecuteAsync(this IClient client, PutDocumentRequest request)
+        public static Task<DocumentHeaderResponse> PerformAsync(this IClient client, PutDocumentRequest request)
         {
             return client.Documents.PutAsync(request);
         }
 
-        public static Task<DocumentHeaderResponse> ExecuteAsync(this IClient client, DeleteDocumentRequest request)
+        public static Task<DocumentHeaderResponse> PerformAsync(this IClient client, DeleteDocumentRequest request)
         {
             return client.Documents.DeleteAsync(request);
         }
 
-        public static Task<AttachmentResponse> ExecuteAsync(this IClient client, GetAttachmentRequest request)
+        public static Task<AttachmentResponse> PerformAsync(this IClient client, GetAttachmentRequest request)
         {
             return client.Attachments.GetAsync(request);
         }
 
-        public static Task<DocumentHeaderResponse> ExecuteAsync(this IClient client, PutAttachmentRequest request)
+        public static Task<DocumentHeaderResponse> PerformAsync(this IClient client, PutAttachmentRequest request)
         {
             return client.Attachments.PutAsync(request);
         }
 
-        public static Task<DocumentHeaderResponse> ExecuteAsync(this IClient client, DeleteAttachmentRequest request)
+        public static Task<DocumentHeaderResponse> PerformAsync(this IClient client, DeleteAttachmentRequest request)
         {
             return client.Attachments.DeleteAsync(request);
         }
 
-        public static Task<EntityResponse<T>> ExecuteAsync<T>(this IClient client, GetEntityRequest request) where T : class
+        public static Task<EntityResponse<T>> PerformAsync<T>(this IClient client, GetEntityRequest request) where T : class
         {
             return client.Entities.GetAsync<T>(request);
         }
