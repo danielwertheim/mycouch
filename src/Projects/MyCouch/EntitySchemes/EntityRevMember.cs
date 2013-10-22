3d8903bb0f7b025039b5c44d6c2b3d597cda2202
Daniel Wertheim <daniel@wertheim.se>
2013-10-22 22:38:12 +0200

    Support for extracting ModelRev from inherited classes.

--- src/Projects/MyCouch/EntitySchemes/EntityRevMember.cs
+++ src/Projects/MyCouch/EntitySchemes/EntityRevMember.cs
@@ -12,22 +12,31 @@ namespace MyCouch.EntitySchemes
 
         public override int? GetMemberRankingIndex(Type entityType, string membername)
         {
+            return GetMemberRankingIndex(entityType, membername, 0);
+        }
+
+        protected virtual int? GetMemberRankingIndex(Type entityType, string membername, int level)
+        {
+            var factor = level <= 0 ? 1 : level;
+
             if (membername.Equals("_rev", StringComparison.OrdinalIgnoreCase))
-                return 0;
+                return 0 * factor;
 
             if (membername.Equals(string.Concat(entityType.Name, "rev"), StringComparison.OrdinalIgnoreCase))
-                return 1;
+                return 1 * factor;
 
             if (membername.Equals("documentrev", StringComparison.OrdinalIgnoreCase))
-                return 2;
+                return 2 * factor;
 
             if (membername.Equals("entityrev", StringComparison.OrdinalIgnoreCase))
-                return 3;
+                return 3 * factor;
 
             if (membername.Equals("rev", StringComparison.OrdinalIgnoreCase))
-                return 4;
+                return 4 * factor;
 
-            return null;
+            return entityType.BaseType == typeof(object)
+                ? null
+                : GetMemberRankingIndex(entityType.BaseType, membername, (factor * 10));
         }
     }
 }
