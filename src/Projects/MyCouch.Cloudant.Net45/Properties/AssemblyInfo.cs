6a4e9c361c4eab501e093dd168a7885af6f5ff96
Daniel Wertheim <daniel@wertheim.se>
2013-10-26 22:46:19 +0200

    MyCouch.Cloudant skeleton projects in place.

---
+++ src/Projects/MyCouch.Cloudant.Net45/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyCouch.Cloudant.Net45")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9d254e0e-e05b-4afb-acfd-6d916c4b5f86")]
