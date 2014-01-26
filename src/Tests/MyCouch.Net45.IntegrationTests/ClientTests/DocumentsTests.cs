947cafccd426db4470592e1038948548a3c24a71
Daniel Wertheim <daniel@wertheim.se>
2014-01-26 17:11:49 +0100

    Clean.

--- src/Tests/MyCouch.Net45.IntegrationTests/ClientTests/DocumentsTests.cs
+++ src/Tests/MyCouch.Net45.IntegrationTests/ClientTests/DocumentsTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using FluentAssertions;
 using MyCouch.Testing;
 using MyCouch.Testing.TestData;
