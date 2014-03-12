b372669679f9dbfff6e14cec10bd57a640f66dfa
Daniel Wertheim <daniel@wertheim.se>
2014-03-12 21:21:38 +0100

    Clean output of exception.

--- src/Projects/MyCouch.Net45/MyCouchException.cs
+++ src/Projects/MyCouch.Net45/MyCouchException.cs
@@ -16,7 +16,7 @@ namespace MyCouch
 
         public MyCouchException(HttpMethod httpMethod, HttpStatusCode httpStatus, Uri uri, string error, string reason)
             : base(string.Format(
-                "MyCouch failed.{0}HttpMethod: {1}.{0}HttpStatus: {2}.{0}Uri:{3}.{0}Error: {4}.{0}Reason: {5}.{0}",
+                "MyCouch failed.{0}HttpMethod: {1}{0}HttpStatus: {2}{0}Uri:{3}{0}Error: {4}{0}Reason: {5}{0}",
                 Environment.NewLine,
                 httpMethod,
                 httpStatus,
