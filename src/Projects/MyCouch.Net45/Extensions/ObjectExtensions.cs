37bd9fcf252dea54c88966232c060d928940feeb
Daniel Wertheim <daniel@wertheim.se>
2014-04-10 20:15:33 +0200

    Internal stuff.

--- src/Projects/MyCouch.Net45/Extensions/ObjectExtensions.cs
+++ src/Projects/MyCouch.Net45/Extensions/ObjectExtensions.cs
@@ -1,13 +1,13 @@
 ﻿namespace MyCouch.Extensions
 {
-    public static class ObjectExtensions
+    internal static class ObjectExtensions
     {
-        public static T To<T>(this object value)
+        internal static T To<T>(this object value)
         {
             return (T) value;
         }
 
-        public static bool IsNumeric(this object value)
+        internal static bool IsNumeric(this object value)
         {
             return
                 value is short || value is int || value is long || value is ushort || value is uint || value is ulong ||
