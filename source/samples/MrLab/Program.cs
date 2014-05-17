5d2ccba514afe4030882aa8399bdfa451b4f0e10
Daniel Wertheim <daniel@wertheim.se>
2014-05-17 23:58:06 +0200

    MrLab

---
+++ source/samples/MrLab/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyCouch;
+
+namespace MrLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new MyCouchClient("http://sa:test@localhost:5984/a"))
+            {
+
+            }
+        }
+    }
+}
