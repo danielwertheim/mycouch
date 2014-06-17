4d3ef018e3ce8539bda0381e7e673fdbc1ac57bc
Daniel Wertheim <daniel@wertheim.se>
2014-06-17 20:37:08 +0200

    Fixed wrong version

--- source/SharedAssemblyInfo.cs
+++ source/SharedAssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.2.0*")]
+[assembly: AssemblyVersion("2.2.0.*")]
 [assembly: AssemblyFileVersion("2.2.0")]
