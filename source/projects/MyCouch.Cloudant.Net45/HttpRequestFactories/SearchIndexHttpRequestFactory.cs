78bd75e3912b4bf039547eb4ea9f2bdc527b1083
indranilatcal <in201174>
2014-08-29 01:51:00 +0530

    Modified cloudant bootstraper to include a normal serializer that doesn't depend on generic type.

--- source/projects/MyCouch.Cloudant.Net45/HttpRequestFactories/SearchIndexHttpRequestFactory.cs
+++ source/projects/MyCouch.Cloudant.Net45/HttpRequestFactories/SearchIndexHttpRequestFactory.cs
@@ -9,91 +9,95 @@ using MyCouch.Serialization;
 
 namespace MyCouch.Cloudant.HttpRequestFactories
 {
-	public class SearchIndexHttpRequestFactory
-	{
-		protected ISerializer Serializer { get; private set; }
+    public class SearchIndexHttpRequestFactory
+    {
+        protected ISerializer Serializer { get; private set; }
+        protected ISerializer NormalSerializer { get; private set; }
 
-		public SearchIndexHttpRequestFactory(ISerializer serializer)
-		{
-			Ensure.That(serializer, "serializer").IsNotNull();
+        public SearchIndexHttpRequestFactory(ISerializer serializer, ISerializer normalSerializer)
+        {
+            Ensure.That(serializer, "serializer").IsNotNull();
+            Ensure.That(normalSerializer, "normalSerializer").IsNotNull();
 
-			Serializer = serializer;
-		}
 
-		public virtual HttpRequest Create(SearchIndexRequest request)
-		{
-			Ensure.That(request, "request").IsNotNull();
+            Serializer = serializer;
+            NormalSerializer = normalSerializer;
+        }
 
-			return new HttpRequest(HttpMethod.Get, GenerateRelativeUrl(request))
-				.SetRequestTypeHeader(request.GetType());
-		}
+        public virtual HttpRequest Create(SearchIndexRequest request)
+        {
+            Ensure.That(request, "request").IsNotNull();
 
-		protected virtual string GenerateRelativeUrl(SearchIndexRequest request)
-		{
-			return string.Format("/_design/{0}/_search/{1}{2}",
-				request.IndexIdentity.DesignDocument,
-				request.IndexIdentity.Name,
-				GenerateRequestUrlQueryString(request));
-		}
+            return new HttpRequest(HttpMethod.Get, GenerateRelativeUrl(request))
+                .SetRequestTypeHeader(request.GetType());
+        }
 
-		protected virtual string GenerateRequestUrlQueryString(SearchIndexRequest request)
-		{
-			var p = GenerateQueryStringParams(request);
+        protected virtual string GenerateRelativeUrl(SearchIndexRequest request)
+        {
+            return string.Format("/_design/{0}/_search/{1}{2}",
+                request.IndexIdentity.DesignDocument,
+                request.IndexIdentity.Name,
+                GenerateRequestUrlQueryString(request));
+        }
 
-			return string.IsNullOrEmpty(p) ? string.Empty : string.Concat("?", p);
-		}
+        protected virtual string GenerateRequestUrlQueryString(SearchIndexRequest request)
+        {
+            var p = GenerateQueryStringParams(request);
 
-		protected virtual string GenerateQueryStringParams(SearchIndexRequest request)
-		{
-			return string.Join("&", GenerateJsonCompatibleKeyValues(request)
-				.Select(kv => string.Format("{0}={1}", kv.Key, Uri.EscapeDataString(kv.Value))));
-		}
+            return string.IsNullOrEmpty(p) ? string.Empty : string.Concat("?", p);
+        }
 
-		protected virtual IDictionary<string, string> GenerateJsonCompatibleKeyValues(SearchIndexRequest request)
-		{
-			var kvs = new Dictionary<string, string>();
+        protected virtual string GenerateQueryStringParams(SearchIndexRequest request)
+        {
+            return string.Join("&", GenerateJsonCompatibleKeyValues(request)
+                .Select(kv => string.Format("{0}={1}", kv.Key, Uri.EscapeDataString(kv.Value))));
+        }
 
-			if (!string.IsNullOrWhiteSpace(request.Expression))
-				kvs.Add(KeyNames.Expression, request.Expression);
+        protected virtual IDictionary<string, string> GenerateJsonCompatibleKeyValues(SearchIndexRequest request)
+        {
+            var kvs = new Dictionary<string, string>();
 
-			if (request.HasSortings())
-				kvs.Add(KeyNames.Sort, Serializer.ToJsonArray(request.Sort.ToArray()));
+            if (!string.IsNullOrWhiteSpace(request.Expression))
+                kvs.Add(KeyNames.Expression, request.Expression);
 
-			if (!string.IsNullOrWhiteSpace(request.Bookmark))
-				kvs.Add(KeyNames.Bookmark, request.Bookmark);
+            if (request.HasSortings())
+                kvs.Add(KeyNames.Sort, Serializer.ToJsonArray(request.Sort.ToArray()));
 
-			if (request.Stale.HasValue)
-				kvs.Add(KeyNames.Stale, request.Stale.Value.AsString());
+            if (!string.IsNullOrWhiteSpace(request.Bookmark))
+                kvs.Add(KeyNames.Bookmark, request.Bookmark);
 
-			if (request.Limit.HasValue)
-				kvs.Add(KeyNames.Limit, Serializer.ToJson(request.Limit.Value));
+            if (request.Stale.HasValue)
+                kvs.Add(KeyNames.Stale, request.Stale.Value.AsString());
 
-			if (request.IncludeDocs.HasValue)
-				kvs.Add(KeyNames.IncludeDocs, Serializer.ToJson(request.IncludeDocs.Value));
-			
-			if (request.Ranges != null)
-				kvs.Add(KeyNames.Ranges, Serializer.Serialize(request.Ranges));
+            if (request.Limit.HasValue)
+                kvs.Add(KeyNames.Limit, Serializer.ToJson(request.Limit.Value));
 
-			if (request.HasCounts())
-				kvs.Add(KeyNames.Counts, Serializer.ToJsonArray(request.Counts.ToArray()));
+            if (request.IncludeDocs.HasValue)
+                kvs.Add(KeyNames.IncludeDocs, Serializer.ToJson(request.IncludeDocs.Value));
+            
+            if (request.Ranges != null)
+                kvs.Add(KeyNames.Ranges, NormalSerializer.Serialize(request.Ranges));
 
-			if (!string.IsNullOrWhiteSpace(request.GroupField))
-				kvs.Add(KeyNames.GroupField, request.GroupField);
+            if (request.HasCounts())
+                kvs.Add(KeyNames.Counts, Serializer.ToJsonArray(request.Counts.ToArray()));
 
-			return kvs;
-		}
+            if (!string.IsNullOrWhiteSpace(request.GroupField))
+                kvs.Add(KeyNames.GroupField, request.GroupField);
 
-		protected static class KeyNames
-		{
-			public const string Expression = "q";
-			public const string Sort = "sort";
-			public const string Bookmark = "bookmark";
-			public const string Stale = "stale";
-			public const string Limit = "limit";
-			public const string IncludeDocs = "include_docs";
-			public const string Ranges = "ranges";
-			public const string Counts = "counts";
-			public const string GroupField = "group_field";
-		}
-	}
+            return kvs;
+        }
+
+        protected static class KeyNames
+        {
+            public const string Expression = "q";
+            public const string Sort = "sort";
+            public const string Bookmark = "bookmark";
+            public const string Stale = "stale";
+            public const string Limit = "limit";
+            public const string IncludeDocs = "include_docs";
+            public const string Ranges = "ranges";
+            public const string Counts = "counts";
+            public const string GroupField = "group_field";
+        }
+    }
 }
