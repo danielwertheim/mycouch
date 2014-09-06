a6d9ea09d6cf718eab963bf363c3d54743eb243a
Daniel Wertheim <daniel@wertheim.se>
2014-09-06 19:46:26 +0200

    Bumped to v2.4.0

--- source/SharedAssemblyInfo.cs
+++ source/SharedAssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.3.0.*")]
-[assembly: AssemblyFileVersion("2.3.0")]
+[assembly: AssemblyVersion("2.4.0.*")]
+[assembly: AssemblyFileVersion("2.4.0")]
