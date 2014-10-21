fef7924ad5adc94095c8e69a1544bf89d444bab2
HCanber <hakan_github@canberger.se>
2014-10-21 11:14:47 +0200

    Add overloads for QueryAsync with CancellationToken support

--- source/projects/MyCouch.Net45/Contexts/Views.cs
+++ source/projects/MyCouch.Net45/Contexts/Views.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using EnsureThat;
 using MyCouch.Extensions;
@@ -37,11 +38,31 @@ namespace MyCouch.Contexts
             }
         }
 
+        public virtual async Task<RawResponse> QueryRawAsync(QueryViewRequest request, CancellationToken cancellationToken)
+        {
+            var httpRequest = CreateHttpRequest(request);
+
+            using (var res = await SendAsync(httpRequest, cancellationToken))
+            {
+                return ProcessRawHttpResponse(res);
+            }
+        }
+
         public virtual async Task<ViewQueryResponse> QueryAsync(QueryViewRequest request)
         {
             var httpRequest = CreateHttpRequest(request);
 
             using (var res = await SendAsync(httpRequest).ForAwait())
+            {
+                return ProcessHttpResponse(res);
+            }
+        }
+
+        public virtual async Task<ViewQueryResponse> QueryAsync(QueryViewRequest request, CancellationToken cancellationToken)
+        {
+            var httpRequest = CreateHttpRequest(request);
+
+            using (var res = await SendAsync(httpRequest, cancellationToken).ForAwait())
             {
                 return ProcessHttpResponse(res);
             }
@@ -57,11 +78,31 @@ namespace MyCouch.Contexts
             }
         }
 
+        public virtual async Task<ViewQueryResponse<TValue>> QueryAsync<TValue>(QueryViewRequest request, CancellationToken cancellationToken)
+        {
+            var httpRequest = CreateHttpRequest(request);
+
+            using (var res = await SendAsync(httpRequest, cancellationToken).ForAwait())
+            {
+                return ProcessHttpResponse<TValue>(res);
+            }
+        }
+
         public virtual async Task<ViewQueryResponse<TValue, TIncludedDoc>> QueryAsync<TValue, TIncludedDoc>(QueryViewRequest request)
         {
             var httpRequest = CreateHttpRequest(request);
 
             using (var res = await SendAsync(httpRequest).ForAwait())
+            {
+                return ProcessHttpResponse<TValue, TIncludedDoc>(res);
+            }
+        }
+
+        public virtual async Task<ViewQueryResponse<TValue, TIncludedDoc>> QueryAsync<TValue, TIncludedDoc>(QueryViewRequest request, CancellationToken cancellationToken)
+        {
+            var httpRequest = CreateHttpRequest(request);
+
+            using (var res = await SendAsync(httpRequest, cancellationToken).ForAwait())
             {
                 return ProcessHttpResponse<TValue, TIncludedDoc>(res);
             }
--- source/projects/MyCouch.Net45/IViews.cs
+++ source/projects/MyCouch.Net45/IViews.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using MyCouch.Requests;
 using MyCouch.Responses;
 
@@ -17,6 +18,14 @@ namespace MyCouch
         Task<RawResponse> QueryRawAsync(QueryViewRequest request);
 
         /// <summary>
+        /// Lets you perform a query, that can be cancelled, and get the result as RAW JSON.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken">A cancellation token that can be used to cancel the query</param>
+        /// <returns></returns>
+        Task<RawResponse> QueryRawAsync(QueryViewRequest request, CancellationToken cancellationToken);
+
+        /// <summary>
         /// Lets you perform a query by using a reusable <see cref="QueryViewRequest"/>.
         /// Any returned Value and, or IncludedDoc of the response,
         /// will be treated as JSON-strings.
@@ -24,6 +33,16 @@ namespace MyCouch
         /// <param name="query"></param>
         /// <returns></returns>
         Task<ViewQueryResponse> QueryAsync(QueryViewRequest query);
+
+        /// <summary>
+        /// Lets you perform a query by using a reusable <see cref="QueryViewRequest"/>.
+        /// Any returned Value and, or IncludedDoc of the response,
+        /// will be treated as JSON-strings.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="cancellationToken">A cancellation token that can be used to cancel the query</param>
+        /// <returns></returns>
+        Task<ViewQueryResponse> QueryAsync(QueryViewRequest query, CancellationToken cancellationToken);
 
         /// <summary>
         /// Lets you perform a query by using a reusable <see cref="QueryViewRequest"/>.
@@ -39,6 +58,17 @@ namespace MyCouch
         /// Lets you perform a query by using a reusable <see cref="QueryViewRequest"/>.
         /// Any returned Value of the response,
         /// will be treated as defined by <typeparamref name="TValue"/>.
+        /// </summary>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="cancellationToken">A cancellation token that can be used to cancel the query</param>
+        /// <returns></returns>
+        Task<ViewQueryResponse<TValue>> QueryAsync<TValue>(QueryViewRequest query, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Lets you perform a query by using a reusable <see cref="QueryViewRequest"/>.
+        /// Any returned Value of the response,
+        /// will be treated as defined by <typeparamref name="TValue"/>.
         /// Any returned IncludedDoc of the response,
         /// will be treated as defined by <typeparamref name="TIncludedDoc"/>.
         /// </summary>
@@ -47,5 +77,19 @@ namespace MyCouch
         /// <param name="query"></param>
         /// <returns></returns>
         Task<ViewQueryResponse<TValue, TIncludedDoc>> QueryAsync<TValue, TIncludedDoc>(QueryViewRequest query);
+
+        /// <summary>
+        /// Lets you perform a query by using a reusable <see cref="QueryViewRequest"/>.
+        /// Any returned Value of the response,
+        /// will be treated as defined by <typeparamref name="TValue"/>.
+        /// Any returned IncludedDoc of the response,
+        /// will be treated as defined by <typeparamref name="TIncludedDoc"/>.
+        /// </summary>
+        /// <typeparam name="TValue"></typeparam>
+        /// <typeparam name="TIncludedDoc"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="cancellationToken">A cancellation token that can be used to cancel the query</param>
+        /// <returns></returns>
+        Task<ViewQueryResponse<TValue, TIncludedDoc>> QueryAsync<TValue, TIncludedDoc>(QueryViewRequest query, CancellationToken cancellationToken);
     }
 }
